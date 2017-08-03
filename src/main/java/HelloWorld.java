08117b6f622d42249b9346261b7ee46f7a7ec49c
‘SunFuGithub’ <2569768497@qq.com>
2017-08-03 18:49:03 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by iFunk on 2017/8/3.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hellow world!!!!!");
+    }
+
+    public void a() {
+
+    }
+}
